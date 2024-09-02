f29ffc5dc374a47cdf1e7c37be16959677095fe5
billytoshi <42888772+billytoshi@users.noreply.github.com>
2024-09-02 14:26:44 +0700

    Update 03_build.sh

--- code/ci_actions/03_build.sh
+++ code/ci_actions/03_build.sh
@@ -25,14 +25,7 @@ plutil -replace CFBundleVersion -string $BUILD_NUMBER "./getting started/Info.pl
 
 security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 
-# xcodebuild clean archive                    \
-#            -project  "$PROJECT"             \
-#            -scheme "$SCHEME"                \
-#            -archivePath "$ARCHIVE_PATH"     \
-#            -derivedDataPath "${BUILD_PATH}" \
-#            -configuration "$CONFIGURATION"   | $BREW_PATH/xcbeautify
-
-xcodebuild clean                    \
+xcodebuild clean archive                    \
            -project  "$PROJECT"             \
            -scheme "$SCHEME"                \
            -archivePath "$ARCHIVE_PATH"     \
